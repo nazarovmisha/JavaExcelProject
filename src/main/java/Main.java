5ac6715a025e7fab2a3881dae66c44b6fc7ab910
Nazarov Mikhail <nazarovmisha@inbox.ru>
2023-01-17 14:39:16 +0300

    continuing development

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main {
     public static void main(String[] args) {
+
+        XSSFWorkbook xssfWorkbook= new XSSFWorkbook();
+        xssfWorkbook.getSheet("Студенты");
+
 
         University university = new University();
         university.setId("1")
